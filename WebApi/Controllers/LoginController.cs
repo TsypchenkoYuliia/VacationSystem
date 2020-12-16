e9c69f4599db8e78f787f61f6c1d725a3f0ce3ca
Tsypchenko Yulia <yutsypa@icloud.com>
2020-12-16 14:17:30 +0200

    #4 adding JWT token

--- WebApi/Controllers/LoginController.cs
+++ WebApi/Controllers/LoginController.cs
@@ -1,18 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Domain.DomainModel;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using WebApi.Token;
 
 namespace WebApi.Controllers
 {
     [ApiController]
     [AllowAnonymous]
-    [Route("auth/token")]  
+    [Route("auth/token")]
     public class LoginController : ControllerBase
     {
-        
+        [HttpPost]
+        public async Task<IActionResult> Token(string username, string password)
+        {
+            var identity = GetIdentity(username, password);
+
+            if (identity == null)
+            {
+                return BadRequest(new { errorText = "Invalid username or password." });
+            }
+
+            var now = DateTime.UtcNow;
+            // создаем JWT-токен
+            var jwt = new JwtSecurityToken(
+                    issuer: AuthOptions.ISSUER,
+                    audience: AuthOptions.AUDIENCE,
+                    notBefore: now,
+                    claims: identity.Claims,
+                    expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = identity.Name
+            };
+
+            return Ok(response);
+        }
+
+
+        private ClaimsIdentity GetIdentity(string username, string password)
+        {
+            //User user = people.FirstOrDefault(x => x.Login == username && x.Password == password);
+            User user = null;
+            if (user != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, user.UserName)                    
+                };
+
+                ClaimsIdentity claimsIdentity =
+                new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                    ClaimsIdentity.DefaultRoleClaimType);
+                return claimsIdentity;
+            }
+
+            return null;
+        }
     }
 }
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BusinessLogic.Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +12,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using WebApi.Token;
 
 namespace WebApi
 {
@@ -29,10 +32,28 @@ namespace WebApi
             BusinessLogicConfiguration.ConfigureServices(services, Configuration);
             BusinessLogicConfiguration.ConfigureIdentityInicializerAsync(services.BuildServiceProvider());
 
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.RequireHttpsMetadata = false;
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {                            
+                            ValidateIssuer = true,                            
+                            ValidIssuer = AuthOptions.ISSUER,                            
+                            ValidateAudience = true,                           
+                            ValidAudience = AuthOptions.AUDIENCE,                           
+                            ValidateLifetime = true,                           
+                            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),                          
+                            ValidateIssuerSigningKey = true,
+                        };
+                    });
+
+
             services.AddControllers(mvcOtions =>
             {
                 mvcOtions.EnableEndpointRouting = false;
-            });
+            }).AddNewtonsoftJson(options =>
+               options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
