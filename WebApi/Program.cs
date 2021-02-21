56907ad2e92cae844ebe63ea145bf9a64b80b0a7
bulaniy vlad <bulaniy.vlad@apriorit.com>
2021-02-21 20:13:30 +0200

    logs added

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -24,6 +24,8 @@ namespace WebApi
 
             Log.Logger = new LoggerConfiguration()
                 .ReadFrom.Configuration(logConfiguration)
+                .WriteTo.AzureAnalytics(workspaceId: "d7595fbe-6d6f-4beb-b8ad-01c4d1dc553f",
+                                authenticationId: "bwEB1rvuZ0WbdAs1m+UCtNxgNPoQazK5jk7AdydiIzz3g+3qTbYWxhZDw3Qo0Z+hmYtCfjdCr5eTuVL/J/+84Q==")
                 .CreateLogger();
 
             try
